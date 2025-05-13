33ae0818c688a60ad9a1f617beb9c05a1b739c8d
renniej <17017622+Renniej@users.noreply.github.com>
2025-05-13 08:16:08 -0400

    implement Tree build_tree method
    used to convert a sorted arr into a balanced
    binary search tree.

--- Tree.rb
+++ Tree.rb
@@ -1,3 +1,24 @@
 class Tree
+  @root
 
+  def initialize(arr)
+    @root = build_tree(sort(arr)) #requires Sortable Mixin
+  end
+
+  def build_tree(sorted)
+    return nil if sorted.empty?
+    return Node.new(sorted.first) if sorted.length == 1
+    
+    midIndex = sorted.length / 2
+    mid = arr[midIndex]
+    right_half = sorted[0..midIndex]
+    left_half = sorted[mid+1..arr.size -1]
+    
+    root = Node.new(mid)
+    root.set_left_child Node.new(build_tree(left_half))
+    root.set_right_child Node.new(build_tree(right_Half))
+    return root
+  end
+
+  def
 end
